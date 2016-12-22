8f4963700e587f78bc8c3b0de02fe1662c578aff
Jing2684 <1843249292@qq.com>
2016-12-22 18:02:26 +0800

    add a test

---
+++ DispatchSemaphoresDemo/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  DispatchSemaphoresDemo
+//
+//  Created by jing on 16/12/22.
+//  Copyright © 2016年 jing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
++ (void)justTest;
+@end
---
+++ DispatchSemaphoresDemo/Test.m
@@ -0,0 +1,15 @@
+//
+//  Test.m
+//  DispatchSemaphoresDemo
+//
+//  Created by jing on 16/12/22.
+//  Copyright © 2016年 jing. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
++ (void)justTest{
+    NSLog(@"DDD");
+}
+@end
